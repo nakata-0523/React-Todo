cb853780fb2f7d599d2ab6b709fec77c23ec1b58
nakata-0523 <65937778+nakata-0523@users.noreply.github.com>
2021-11-09 20:47:35 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import React, {Component} from 'react';
 
-// class App extends Component {
-//   render() {
-
-//     //return内は一つだけにしないといけないのでタグで囲って一つの塊にする
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={() => {console.log("I am Changed")}} />
-//       </React.Fragment>
-//     )
-//   }
-// }
-
 const App = () => {
+  const profile = [
+    { name:"taro", age:21, tex:"I like cat." },
+    { name:"hanako", age:2, tex:"I like dog." },
+    {}
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profile.map((profile) => {
+          return <User name={profile.name} age={profile.age} text={profile.tex}/>
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>私の名前は{props.name}です。{props.age}才です。{props.text}</div>
+}
+
+User.defaultProps = {
+  name: "NoName",
+  age: 100,
+  text: "No text"
 }
 
 export default App;
