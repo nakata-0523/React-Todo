cec3b848813b5c5d513f477c1c6d2b2cd088f70d
nakata-0523 <65937778+nakata-0523@users.noreply.github.com>
2021-11-27 22:01:10 +0900

    implement store

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -4,7 +4,7 @@ const initialState = { value: 0 }   //状態の初期値を定義　オブジェ
 
 export default (state = initialState, action) => {  //countReducerを定義　関数として定義　index.jsで使えるようにexport stateは状態
     switch (action.type) {  //actionのtypeを拾うことができる 二種類の値を取れる（INCREMENTとDECREMENT）
-        case INCREMENT:
+        case INCREMENT:     //それぞれの場合の状態の変化の処理
             return { value: state.value + 1 }
         case DECREMENT:
             return { value: state.value - 1 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux'  //結合する機能
 import count from './count'  //count.jsからcountの状態を持つreducerをインポート
 
-export default combineReducers({ count })   //countを結合・のちに他のファイルで使うためexport
-                                            //使うreducerを引数に入れるだけで結合できる
+export default combineReducers({ count })   //countを結合・のちに他のファイルで使うためexport 使うreducerを引数に入れるだけで結合できる
