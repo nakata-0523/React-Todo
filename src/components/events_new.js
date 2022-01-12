490729bc8bdf88a10641100f8e8b3f8406379b56
nakata-0523 <65937778+nakata-0523@users.noreply.github.com>
2022-01-12 11:46:35 +0900

    implement event new component

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,23 +1,61 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form'
 import { Link }　from 'react-router-dom'　　//リンク機能を有するパッケージ
 
-//import { postEvents } from '../actions';
+import { postEvents } from '../actions';
 
 class EventsNew extends Component {
-  render () {
-    return (
-      <React.Fragment>
-          <div>foo</div>
-      </React.Fragment>
-    )
-  }
+    constructor(props) {
+        super(props)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    renderField(field) {
+        const { input, label, type, meta: { touched, error } } = field
+
+        return (
+            <div>
+                <input { ...input } placeholder={ label } type={ type } />
+                { touched && error && <span>{ error }</span> }
+            </div>
+        )
+    }
+
+    async onSubmit(values) {
+        await this.props.postEvents(values)
+        this.props.history.push('/')
+    }
+
+    render () {
+        const { handleSubmit } = this.props
+
+        return (
+            <form onSubmit={ handleSubmit(this.onSubmit) }>
+                <div><Field label="Title" name="title" type="text" component={ this.renderField } /></div>
+                <div><Field label="Body" name="body" type="text" component={ this.renderField } /></div>
+
+                <div>
+                    <input type="submit" value="Submit" disabled={ false } />
+                    <Link to="/">Cancel</Link>
+                </div>
+            </form>
+        )
+    }
 }
 
-//const mapDispatchToProps = ({ postEvents }) //下のコードの省略版
-// const mapDispatchToProps = dispatch => ({       //状態遷移の処理
-//   increment: () => dispatch(increment()),
-//   decrement: () => dispatch(decrement())
-// })
+const validate = values => {
+    const errors = {}
 
-export default connect(null, null)(EventsNew)
+    //入力フォーム（Title, Body）に何も記入がない場合に入力してくださいを表示させる
+    if (!values.title) errors.title = "Error a title, please."
+    if (!values.body) errors.body = "Error a body, please."
+
+    return errors
+}
+
+const mapDispatchToProps = ({ postEvents }) //下のコードの省略版
+
+export default connect(null, mapDispatchToProps)(
+    reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+)
